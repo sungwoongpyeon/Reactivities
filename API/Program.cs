c3ef9fcf744d77e0c8060ac588dd724ee5627e8e
Shawn Pyeon <spyeon@orican.com>
2023-10-09 12:48:17 -0400

    End of walking skeleton API

---
+++ API/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Things that we use in the application
+// Dependency Injections
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// Middleware below
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers(); //This middleware allows to map the http request to the right controller.
+
+//what is using in c#? 
+//when the scope is done, anything inside the scope is going be destroyed or disposed
+//and cleaned up for the memory. 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    // This is the same as "dotnet ef database update" command
+    // We don't need to run the database update command manually. 
+    // This will allow us to run the command automatically when restarting a project all the time.
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migrations");
+}
+
+app.Run();
